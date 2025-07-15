473df391cbc900443041518f8b6951aac333503e
林见夕 <2180727350@qq.com>
2025-07-15 11:00:21 +0800

    Update config.js
    GA4 代码追踪器

--- config.js
+++ config.js
@@ -52,22 +52,39 @@ export default defineUserConfig({
         content: 'X5YSaTDn-pKqQBUKD_05_dQcxVItzEq7Rlbg2ZEU7AM',
       },
     ],
+    // [
+    //   'script',
+    //   {
+    //     src: 'https://www.googletagmanager.com/gtag/js?id=G-LYDB5TTVMB',
+    //     async: true
+    //   }
+    // ],
     [
       'script',
-      {
-        src: 'https://www.googletagmanager.com/gtag/js?id=G-LYDB5TTVMB',
-        async: true
-      }
+      {},
+      `
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MF83PPKC"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+`
     ],
     [
       'script',
       {},
       `
-       window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+    // window.dataLayer = window.dataLayer || [];
+    // function gtag(){dataLayer.push(arguments);}
+    // gtag('js', new Date());
+    // gtag('config', 'G-LYDB5TTVMB');
 
-    gtag('config', 'G-LYDB5TTVMB');
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-MF83PPKC');</script>
+    <!-- End Google Tag Manager -->
       `
     ]
   ],
